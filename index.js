9ca242f40eb158ca4f908602c3274863fed2f5ce
DamsFX <contact@hounddd.fr>
2021-12-11 16:56:59 +0100

    Update for tailwind v3

--- index.js
+++ index.js
@@ -1,35 +1,3 @@
-/*
-export default function({addBase, theme}) {
-  if (process.env.NODE_ENV === "production") return
-
-  const screens = theme('screens', {})
-  const breakpoints = Object.keys(screens)
-
-  addBase({
-    'body::after': {
-      content: `"Current breakpoint default (< ${screens[breakpoints[0]]})"`,
-      position: 'fixed',
-      right: '.5rem', // could replace with theme('spacing.2', '.5rem'), same for most of the other values
-      bottom: '.5rem',
-      padding: '.5rem .5rem .5rem 2rem',
-      background: 'no-repeat .5rem center / 1.25rem url(https://tailwindcss.com/favicon-32x32.png), #edf2f7',
-      border: '1px solid #cbd5e0',
-      color: '#d53f8c',
-      fontSize: '.875rem',
-      fontWeight: '600',
-      zIndex: '99999',
-    },
-    ...breakpoints.reduce((acc, current) => {
-      acc[`@media (min-width: ${screens[current]})`] = {
-        'body::after': {
-          content: `"Current breakpoint ${current}"`
-        }
-      }
-      return acc
-    }, {})
-  })
-}
-*/
 
 module.exports = function ({
   addComponents,
